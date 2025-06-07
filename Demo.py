f19823d1cf10514de04c0e720ee622725d1f27c6
Anhht <hatuandzanh096@gmail.com>
2025-06-07 21:52:47 +0700

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -2,12 +2,14 @@
 import cv2
 import face_recognition
 from face_recognition import face_locations
-
+# Load ảnh từ file pic 2 có sẵn trong thư mục 
 imgElon=face_recognition.load_image_file(r'pic2/elon-musk.jpeg')
 imgElon=cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 
 imgCheck=face_recognition.load_image_file(r'pic2/Eloncheck.jpg')
 imgCheck=cv2.cvtColor(imgCheck,cv2.COLOR_BGR2RGB)
+
+#thực hiện xác định vị trí khuôn mặt và mã hóa 
 
 faceLoc=face_recognition.face_locations(imgElon)[0]
 
@@ -18,7 +20,7 @@ faceCheck=face_recognition.face_locations(imgElon)[0]
 encodeCheck=face_recognition.face_encodings(imgCheck)[0]
 cv2.rectangle(imgCheck,(faceCheck[3],faceCheck[0]),(faceCheck[1],faceCheck[2]),(225,0,225),2)
 
-
+#So sánh dùng hàm có sẵn
 results=face_recognition.compare_faces([encodeElon],encodeCheck)
 print(results)
 
